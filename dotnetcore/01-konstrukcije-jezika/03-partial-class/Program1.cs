2e716bab11a63b3a0f7cdf418f961358a266ea93
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-11-04 13:04:52 +0100

    dodao projekte

---
+++ dotnetcore/01-konstrukcije-jezika/03-partial-class/Program1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloConsole
+{
+    partial class Program
+    {
+        static void prikazDo42()
+        {
+            int i = 40;
+            while (i <= 42)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+    }
+}
