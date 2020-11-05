8467eb6a379c9d1ceca9a69eac521cf13076e0aa
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-11-05 16:42:31 +0100

    dodao jos projekata

---
+++ dotnetcore/01-konstrukcije-jezika/05-init-code/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace HelloConsoleProperties
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Poruka p = new Poruka {
+                BrojPonavljanja = 2, 
+                Sadrzaj = "YYY"};
+            p.PrikazNaKonzolu();
+            p.BrojPonavljanja = 3;
+            p.Sadrzaj = "Miki Maus";
+            p.PrikazNaKonzolu();
+        }
+
+    }
+
+}
---
+++ dotnetcore/01-konstrukcije-jezika/06-generics/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace HelloConsoleGenerics
+{
+    public class Program
+    {
+    static void Main()
+    {
+        // int is the type argument
+        OneWayList<int> list = new OneWayList<int>();
+
+        for (int x = 0; x < 5; x++)
+        {
+            list.AddHead(x);
+        }
+        foreach (int i in list)
+        {
+            System.Console.Write(i + " ");
+        }
+        
+        OneWayList<Poruka> list2 = new OneWayList<Poruka>();
+
+        for (int x = 0; x < 5; x++)
+        {
+            list2.AddHead(new Poruka{
+                Sadrzaj = "Broj" + x,
+                BrojPonavljanja = 1
+            });
+        }
+        foreach (Poruka p in list2)
+        {
+            p.PrikazNaKonzolu();
+        }
+    }
+    }
+
+}
