8c0669144a76f221dfaa76b706446ff4b7a40c89
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-11-08 09:36:17 +0100

    popravio

---
+++ dotnetcore/01-konstrukcije-jezika/async-prog/01-async-await/README.md
@@ -0,0 +1,3 @@
+Preuzeto od Tim Corey.
+
+Detaljne informacije na https://www.youtube.com/watch?v=2moh18sh5p4 
---
+++ dotnetcore/01-konstrukcije-jezika/async-prog/02-async-await-advanced/WebsiteDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WPFUserInterface
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
